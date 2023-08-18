cab491cd84d2fb2822de075eae3269a5b4581845
Marcos Vinicius <viniciusfranca09@hotmail.com>
2023-08-18 16:17:51 -0300

    Primeiro commit

--- src/pages/jogar/Jogar.tsx
+++ src/pages/jogar/Jogar.tsx
@@ -10,7 +10,7 @@ const Jogar = () => {
   const { id } = useParams();
 
   async function conteudos() {
-    const api = await fetch(`https://simplificaa.vercel.app/${id}.json`);
+    const api = await fetch(`https://simplificaa.vercel.app/jogar/${id}.json`);
     const data = await api.json();
     setAssunto(data);
   }
